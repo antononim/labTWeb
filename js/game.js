76448d83fa21933c1e4adc583a15c2f6f1fb2e2e
antononim <antosaos47@gmail.com>
2025-03-09 22:27:32 +0200

    Add login form

--- js/game.js
+++ js/game.js
@@ -2,21 +2,31 @@
 if (localStorage['pineapples'] == undefined) {
 	localStorage['pineapples'] = 0;
 	localStorage['ppc'] = 1;
+	localStorage['pps'] = 1;
 }
 let userStats = {
 	"pineapples": Number(localStorage['pineapples']),
 	"ppc": Number(localStorage['ppc']), // Pineapples per click, ofc!
+	"pps": Number(localStorage['pps'])
 }
 
-let pineappleCount;
-let ppcCount;
+
+let countElements = {
+	"pineappleCount": undefined,
+	"ppcCount": undefined,
+	"ppsCount": undefined
+}
 function init() {
 	document.querySelector("#clickbtn").addEventListener("click", click);
-	pineappleCount = document.querySelector("#pineappleCounter");
-	ppcCount = document.querySelector("#ppcCount");
+	countElements['pineappleCount'] = document.querySelector("#pineappleCounter");
+	countElements['ppcCount'] = document.querySelector("#ppcCount");
+	countElements['ppsCount'] = document.querySelector("#ppsCount");
 
 	setInterval(update, 200);
 	setInterval(autoclick, 1000);
+	setInterval(saveLocally, 10000);
+	setInterval(saveGlobally, 120000);
+	document.addEventListener("onbeforeunload", closing);
 }
 // setTimeout( () , 100 );
 
@@ -28,37 +38,65 @@ function click() {
 function update() {
 	// check for errors in pineapple counter
 	// just zero it out cuz there is currently no way to check what value was there
-	// TODO: make a loop thru userStats obj
-	if (isNaN(userStats.pineapples)) userStats.pineapples = 1;
-	if (isNaN(userStats.ppc)) userStats.ppc = 1;
-	if (userStats.ppc == 0) userStats.ppc = 1;
-	
 
-	if ( pineappleCounter == undefined ) return;
+	// do I REALLY need those bracke{s?
+	for (stat in userStats)
+		if (isNaN(userStats[stat]) || userStats[stat] == 0)
+			userStats[stat] = 1;
+
+
+	if ( countElements['pineappleCount'] == undefined ) return;
 	// assume all nodes are loaded into vars
 	localStorage['pineapples'] = Number(userStats.pineapples);
-	pineappleCount.innerText = Number(userStats.pineapples);
+	countElements['pineappleCount'].innerText = Number(userStats.pineapples);
 
+	// TODO: make a loop thru all count elements?
+
+	countElements['ppcCount'].innerText = Number(userStats.ppc);
+	countElements['ppcCount'].nextElementSibling.innerText = getPrice('ppc')
+	
+	countElements['ppsCount'].innerText = Number(userStats.pps);
+	countElements['ppsCount'].nextElementSibling.innerText = getPrice('pps')
+}
+
+function saveLocally() {
 	localStorage['ppc'] = Number(userStats.ppc);
-	ppcCount.innerText = Number(userStats.ppc);	
+	localStorage['pps'] = Number(userStats.pps);
+}
+
+function saveGlobally() {
+	
+}
+
+function closing() {
+	saveLocally();
+	saveGlobally();
 }
 
 function buyUpgrade(upgradeName) {
+	let price = 0;
 	switch (upgradeName) {
 		case "ppc":
-			let price = getPrice();
+			price = getPrice("ppc");
+
 			if (userStats.pineapples <= price) return;
 			userStats.pineapples -= price;
 			userStats.ppc += 1;
+			break;
+
+		case "pps":
+			price = getPrice("pps");
+			if (userStats.pineapples <= price) return;
+			userStats.pineapples -= price;
+			userStats.pps += 1;
 			break;
 	}
 }
 
 function getPrice(upgradeName) {
-	// TODO
-	return 100;
+	return userStats[upgradeName]*100;
 }
 
-function whatever() {
-	alert(1);
+function autoclick() {
+	userStats.pineapples += userStats.pps;
 }
